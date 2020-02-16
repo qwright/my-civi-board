737e88d9b799ae1a89442367b4d6996f20a1333d
rageypaige <paigeyragey@gmail.com>
2020-02-16 13:01:23 -0800

    tweaked appearance & validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Dummy redirect for now, can be expanded in needed
 function redirect(form) {
-  let cityInput = form.city.value;
-  console.log(cityInput);
-  window.location = "civiboard.html";
+    let cityInput = form.city.value;
+    console.log(cityInput);
+    window.location = "civiboard.html";
 }
