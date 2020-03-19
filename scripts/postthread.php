2a3072985ec7e1cd6cf139d06c758b24db4a4d76
Quinn <qfwright@outlook.com>
2020-03-19 15:38:49 -0700

    postthread now enters into database

--- scripts/postthread.php
+++ scripts/postthread.php
@@ -1,17 +1,28 @@
 <?php
-include("database.php");
+include("dbconnect.php");
+$pdo = dbConnect();
 session_start();
+$insert = "INSERT INTO posts (msg, img, userNo, title) VALUES (?,null,?,?)";
+$stmt = $pdo->prepare($insert);
+$stmt->bindValue(1, $_POST["postmsg"]);
+$stmt->bindValue(2, $_SESSION["userNo"]);
+$stmt->bindValue(3, $_POST["title"]);
+$stmt->execute();
+closeConnection($pdo);
+header("Location: ../civiboard.php");
+/*
 try{
 	$pdo = dbConnect();
-	$insert = "INSERT INTO posts (title, msg, userNo, img, time) VALUES (?,?,?,null,NOW())";
+	$insert = "INSERT INTO posts (msg, img, userNo, title) VALUES (?,null,?,?)";
 	$stmt = $pdo->prepare($insert);
-	$stmt->bindValue(1, $_POST["title"]);
-	$stmt->bindValue(2, $_POST["postmsg"]);
-	$stmt->bindValue(3, $_SESSION["userNo"]);
+	$stmt->bindValue(1, $_POST["postmsg"]);
+	$stmt->bindValue(2, $_SESSION["userNo"]);
+	$stmt->bindValue(3, $_POST["title"]);
+	$stmt->execute();
 	closeConnection($pdo);
 	header("Location: ../civiboard.php");
 }
 catch(PDOException $e){
 	die($e->getMessage());
-}
+}*/
 ?>
